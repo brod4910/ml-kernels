397d75965efb8a533f8bc39d4276b2b6fece47bf
brod4910 <brod4910@gmail.com>
2023-11-22 13:10:26 -0700

    Further understanding of how AVX and transpose work

--- examples/sgemm/sgemm_avx.h
+++ examples/sgemm/sgemm_avx.h
@@ -44,29 +44,39 @@ void sgemm_avx(size_t M, size_t N, size_t K, float alpha, float beta) {
   __m256 row_7 = _mm256_load_ps(a + 56);
 
   __m256 r0_r1_lo = _mm256_unpacklo_ps(row_0, row_1);
-  __m256 r0_r1_hi = _mm256_unpackhi_ps(row_0, row_1);
   __m256 r2_r3_lo = _mm256_unpacklo_ps(row_2, row_3);
-  __m256 r2_r3_hi = _mm256_unpackhi_ps(row_2, row_3);
+  auto shf_r0_r3 = _mm256_shuffle_ps(r0_r1_lo, r2_r3_lo, 0b01001101);
+  auto blend_r0_r3 = _mm256_blend_ps(r0_r1_lo, shf_r0_r3, 0b11001100);
   __m256 r4_r5_lo = _mm256_unpacklo_ps(row_4, row_5);
-  __m256 r4_r5_hi = _mm256_unpackhi_ps(row_4, row_5);
   __m256 r6_r7_lo = _mm256_unpacklo_ps(row_6, row_7);
+  auto shf_r4_r7 = _mm256_shuffle_ps(r4_r5_lo, r6_r7_lo, 0b01001101);
+  auto blend_r4_r7 = _mm256_shuffle_ps(r4_r5_lo, shf_r4_r7, 0b11001100);
+  auto t1 = _mm256_permute2f128_ps(blend_r0_r3, blend_r4_r7, 0b00100000);
+  auto t4 = _mm256_permute2f128_ps(blend_r0_r3, blend_r4_r7, 0b00110001);
+
+  print_matrix(reinterpret_cast<float*>(&t1), 1, 8);
+  print_matrix(reinterpret_cast<float*>(&t4), 1, 8);
+
+  __m256 r0_r1_hi = _mm256_unpackhi_ps(row_0, row_1);
+  __m256 r2_r3_hi = _mm256_unpackhi_ps(row_2, row_3);
+  __m256 r4_r5_hi = _mm256_unpackhi_ps(row_4, row_5);
   __m256 r6_r7_hi = _mm256_unpackhi_ps(row_6, row_7);
 
-  auto r0 = _mm256_shuffle_ps(r0_r1_lo, r2_r3_lo, 0x4E);
-  std::cout << "Shuffle r0-lo & r2-lo: ";
-  print_matrix(reinterpret_cast<float*>(&r0), 1, 8);
-  r0 = _mm256_blend_ps(r0_r1_lo, r0, 0xCC);
-  std::cout << "Blend shf-0-lo & r0-lo: ";
-  print_matrix(reinterpret_cast<float*>(&r0), 1, 8);
-  auto r1 = _mm256_shuffle_ps(r4_r5_lo, r6_r7_lo, 0x4E);
-  std::cout << "Shuffle r4-lo & r6-lo: ";
-  print_matrix(reinterpret_cast<float*>(&r1), 1, 8);
-  r1 = _mm256_blend_ps(r4_r5_lo, r1, 0xCC);
-  std::cout << "Blend shf-4-lo & r1-lo: ";
-  print_matrix(reinterpret_cast<float*>(&r1), 1, 8);
-  auto z0 = _mm256_permute2f128_ps(r0, r1, 0x20);
-  std::cout << "Permute: ";
-  print_matrix(reinterpret_cast<float*>(&z0), 1, 8);
+//  auto r0 = _mm256_shuffle_ps(r0_r1_lo, r2_r3_lo, 0x4E);
+//  std::cout << "Shuffle r0-lo & r2-lo: ";
+//  print_matrix(reinterpret_cast<float*>(&r0), 1, 8);
+//  r0 = _mm256_blend_ps(r0_r1_lo, r0, 0xCC);
+//  std::cout << "Blend shf-0-lo & r0-lo: ";
+//  print_matrix(reinterpret_cast<float*>(&r0), 1, 8);
+//  auto r1 = _mm256_shuffle_ps(r4_r5_lo, r6_r7_lo, 0x4E);
+//  std::cout << "Shuffle r4-lo & r6-lo: ";
+//  print_matrix(reinterpret_cast<float*>(&r1), 1, 8);
+//  r1 = _mm256_blend_ps(r4_r5_lo, r1, 0xCC);
+//  std::cout << "Blend shf-4-lo & r1-lo: ";
+//  print_matrix(reinterpret_cast<float*>(&r1), 1, 8);
+//  auto z0 = _mm256_permute2f128_ps(r0, r1, 0x20);
+//  std::cout << "Permute: ";
+//  print_matrix(reinterpret_cast<float*>(&z0), 1, 8);
   //  auto *a = static_cast<float *>(_mm_malloc(M * K * sizeof(float), 32));
 //  auto *b = static_cast<float *>(_mm_malloc(N * K * sizeof(float), 32));
 //  auto *c = static_cast<float *>(_mm_malloc(M * N * sizeof(float), 32));
--- src/luna_avx/operators/transpose.cpp
+++ src/luna_avx/operators/transpose.cpp
@@ -6,6 +6,18 @@
 #include <immintrin.h>
 /*
  * Transpose AVX Notes:
+ *
+ * Transposed 8x8 matrix
+ *
+ * [ 0  8 16 24 32 40 48 56]
+ * [ 1  9 17 25 33 41 49 57]
+ * [ 2 10 18 26 34 42 50 58]
+ * [ 3 11 19 27 35 43 51 59]
+ * [ 4 12 20 28 36 44 52 60]
+ * [ 5 13 21 29 37 45 53 61]
+ * [ 6 14 22 30 38 46 54 62]
+ * [ 7 15 23 31 39 47 55 63]
+ *
  * We start to by unpacking the lo and hi values from r0 & r1, r2 & r3, etc.
  *
  *                      Unpack hi-lo
@@ -19,61 +31,69 @@
  * [48 49 50 51 52 53 54 55]    [ 48 56 49 57 52 60 53 61 ] r6 & r7 lo
  * [56 57 58 59 60 61 62 63]    [ 50 58 51 59 54 62 55 63 ] r6 & r7 hi
  *
- * shuffle r0_lo and r2_lo shuffle(01 00 11 10)
+ * SHUFFLE: r0_r1_lo and r2_r3_lo (01 00 11 10)
  *
  * [ 0  8  1  9  4  12 5  13 ]      10 11 00 01 | 10 11 00 01
- *                              -> [ 1 9  16 24 |  5 13 20 28 ]
+ *                              -> [ 1  9 16 24 |  5 13 20 28 ]
  * [ 16 24 17 25 20 28 21 29 ]
  *
- * Blend shuffle-r0-r2-lo & r0-lo (11 00 11 00)
+ * BLEND: r0-r1-lo and shuffle-r[0-3]-lo (11 00 11 00)
  *
- * [ 1 9 16 24 5 13 20 28 ]       1 1  0  0   1   1  0  0
+ *
+ * [ 0 8  1  9  4 12  5 13 ]      0  0  1  1   0   0  1  1
  *                          ->  [ 0 8 16 24 | 4  12 20 28 ]
- * [ 0 8 1  9  4 12 5  13 ]
+ * [ 1 9 16 24  5 13 20 28 ]
  *
- * shuffle r4_lo and r6_lo shuffle(01 00 11 10)
+ * SHUFFLE: r4_r5_lo and r6_r7_lo  (01 00 11 10)
  *
  * [ 32 40 33 41 36 44 32 45 ]       10 11 00 01   10 11 00 01
  *                              -> [ 33 41 48 56 | 32 45 52 60]
  * [ 48 56 49 57 52 60 53 61 ]
  *
- * Blend shuffle-r4-r6-lo & r1-lo (11 00 11 00)
+ * BLEND: r4-r5-lo and shf r[4-7]-lo (11 00 11 00)
  *
- * [ 1 9 16 24 5 13 20 28 ]       1 1  0  0   1   1  0  0
- *                          ->  [ 0 8 16 24 | 4  12 20 28 ]
- * [ 0 8 1  9  4 12 5  13 ]
+ * [ 32 40 33 41 36 44 32 45 ]         0  0  1  1    0  0  1  1
+ *                              ->  [ 32 40 48 56 | 36 44 52 60 ]
+ * [ 33 41 48 56 32 45 52 60 ]
+ *
+ * PERMUTE: blend(r0-r1-lo & shf-r[0-3]-lo) and blend(r4-r5-lo & shf r[4-7]-lo) (00100000)
+ *
+ *  [ 0  8  16 24 4  12 20 28 ]        00 00         10 00
+ *                              -> [ 0 8 16 24 | 32 40 48 56 ]
+ *  [ 32 40 48 56 36 44 52 60 ]
  *
  * Transposed 8x8 matrix
- * [ 0  8 16 24 32 40 48 56]
- * [ 1  9 17 25 33 41 49 57]
- * [ 2 10 18 26 34 42 50 58]
- * [ 3 11 19 27 35 43 51 59]
- * [ 4 12 20 28 36 44 52 60]
- * [ 5 13 21 29 37 45 53 61]
- * [ 6 14 22 30 38 46 54 62]
- * [ 7 15 23 31 39 47 55 63]
+ * [ 0  8 16 24 32 40 48 56] t0
+ * [ 1  9 17 25 33 41 49 57] t1
+ * [ 2 10 18 26 34 42 50 58] t2
+ * [ 3 11 19 27 35 43 51 59] t3
+ * [ 4 12 20 28 36 44 52 60] t4
+ * [ 5 13 21 29 37 45 53 61] t5
+ * [ 6 14 22 30 38 46 54 62] t6
+ * [ 7 15 23 31 39 47 55 63] t7
  */
 
 
 void transpose_8x8(const float* a, size_t i, float* b, size_t j) {
-  __m256 row_0 = _mm256_load_ps(a + i);
-  __m256 row_1 = _mm256_load_ps(a + i + 8);
-  __m256 row_2 = _mm256_load_ps(a + i + 16);
-  __m256 row_3 = _mm256_load_ps(a + i + 24);
-  __m256 row_4 = _mm256_load_ps(a + i + 32);
-  __m256 row_5 = _mm256_load_ps(a + i + 40);
-  __m256 row_6 = _mm256_load_ps(a + i + 48);
-  __m256 row_7 = _mm256_load_ps(a + i + 56);
+  __m256 row_0 = _mm256_load_ps(a);
+  __m256 row_1 = _mm256_load_ps(a + 8);
+  __m256 row_2 = _mm256_load_ps(a + 16);
+  __m256 row_3 = _mm256_load_ps(a + 24);
+  __m256 row_4 = _mm256_load_ps(a + 32);
+  __m256 row_5 = _mm256_load_ps(a + 40);
+  __m256 row_6 = _mm256_load_ps(a + 48);
+  __m256 row_7 = _mm256_load_ps(a + 56);
 
-  __m256 t0 = _mm256_unpacklo_ps(row_0, row_1);
-  __m256 t1 = _mm256_unpackhi_ps(row_0, row_1);
-  __m256 t2 = _mm256_unpacklo_ps(row_2, row_3);
-  __m256 t3 = _mm256_unpackhi_ps(row_2, row_3);
-  __m256 t4 = _mm256_unpacklo_ps(row_4, row_5);
-  __m256 t5 = _mm256_unpackhi_ps(row_4, row_5);
-  __m256 t6 = _mm256_unpacklo_ps(row_6, row_7);
-  __m256 t7 = _mm256_unpackhi_ps(row_6, row_7);
-
+  __m256 r0_r1_lo = _mm256_unpacklo_ps(row_0, row_1);
+  __m256 r2_r3_lo = _mm256_unpacklo_ps(row_2, row_3);
+  auto shf_r0_r3 = _mm256_shuffle_ps(r0_r1_lo, r2_r3_lo, 0b01001101);
+  auto blend_r0_r3 = _mm256_blend_ps(r0_r1_lo, shf_r0_r3, 0b11001100);
+  __m256 r4_r5_lo = _mm256_unpacklo_ps(row_4, row_5);
+  __m256 r6_r7_lo = _mm256_unpacklo_ps(row_6, row_7);
+  auto shf_r4_r7 = _mm256_shuffle_ps(r4_r5_lo, r6_r7_lo, 0b01001101);
+  auto blend_r4_r7 = _mm256_shuffle_ps(r4_r5_lo, shf_r4_r7, 0b11001100);
+  auto t1 = _mm256_permute2f128_ps(blend_r0_r3, blend_r4_r7, 0b00100000);
+  auto t4 = _mm256_permute2f128_ps(blend_r0_r3, blend_r4_r7, 0b00110001);
 }
 
 void transpose(const float *__restrict__ a, float * __restrict__ b, size_t M, size_t N) {
