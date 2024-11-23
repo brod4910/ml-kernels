a24eb8c08efc3fd90426c55b3156da749e943b07
brod4910 <brod4910@gmail.com>
2024-11-22 23:23:12 -0700

    a few unrefined comments and start implementation of warp tiling

--- include/mlkl/cuda/operators/gemm.h
+++ include/mlkl/cuda/operators/gemm.h
@@ -62,6 +62,9 @@ __global__ void sgemm_v2(const float *a, float alpha, const float *b, float beta
   c[y * N + x] = accumulator;
 }
 
+/*
+Proper block tiling of A & B loading tiles from global to shared memory.
+*/
 __global__ void
 sgemm_v3(const float *a, float alpha, const float *b, float beta, float *c, size_t M, size_t N, size_t K) {
   __shared__ float ATile[TILE_Y][TILE_X];
@@ -113,6 +116,10 @@ sgemm_v3(const float *a, float alpha, const float *b, float beta, float *c, size
   c[linear] = accumulator;
 }
 
+/*
+  There is a lot packed into this function with another another level of tiling.
+  This function implements thread-tiling by calculating multiple output values per thread.
+*/
 // TODO: Delete this and make functions templates
 #define BLOCK_TILE_X 64
 #define BLOCK_TILE_Y 64
@@ -129,12 +136,103 @@ __global__ void sgemm_v4(const float *a, float alpha, const float *b, float beta
   int block_x = blockIdx.x;
   int block_y = blockIdx.y;
 
+  // Dictates which starting value of C we're computing within the C-block
+  // since we're implementing thread-tiling, each thread computes (m, n)
+  // outputs of C.
+  int tid_x = threadIdx.x;
+  int tid_y = threadIdx.y;
+
+  // Local A and B memory fragments we're using to compute our C output thread-tile
+  float a_frag[NUM_TH_ITEMS_M];
+  float b_frag[NUM_TH_ITEMS_N];
+  float accumulator[NUM_TH_ITEMS_M][NUM_TH_ITEMS_N] = {0.0};
+
+  // Block of code is similar to our V3. We first compute the starting points
+  // of our A and B tiles for easier accessing when loading from GMEM.
+  for (int step = 0; step < K / BLOCK_TILE_X; ++step) {
+    // still in GMEM here since we're only computing the starting addresses
+    const float *tile_a = &a[(block_y * BLOCK_TILE_X) * K + (step * BLOCK_TILE_X)];
+    const float *tile_b = &b[(step * BLOCK_TILE_X) * N + (block_x * BLOCK_TILE_X)];
+
+    // Since each thread calculates more than one output value,
+    // we need to also account for this when we load to SMEM.
+    // We don't have enough threads launched in our blocks to load the values
+    // we need from GMEM so this loop accounts for that using the number of
+    // items computed by each thread (M, N)
+    for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+      for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
+        ATile[tid_y + (j * blockDim.x)][tid_x + (i * blockDim.y)] = tile_a[(tid_y + (j * blockDim.x)) * K + ((tid_x + (i * blockDim.y)))];
+        BTile[tid_y + (j * blockDim.x)][tid_x + (i * blockDim.y)] = tile_b[(tid_y + (j * blockDim.x)) * N + ((tid_x + (i * blockDim.y)))];
+      }
+    }
+
+    __syncthreads();
+
+    // Now we need to process the items in SMEM by moving them to
+    // our fragment arrays which are thread-local. We move m items
+    // to a_frag and n items to b_frag. This will give us a vector of
+    // (m, 1) and (1, n) since we're accessing A column-wise and B row-wise.
+    for (int k = 0; k < BLOCK_TILE_X; ++k) {
+      for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
+        a_frag[i] = ATile[tid_y * NUM_TH_ITEMS_M + i][k];
+      }
+      for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+        b_frag[j] = BTile[k][tid_x * NUM_TH_ITEMS_N + j];
+      }
+
+      // Items are now at the thread-level so we can finally compute
+      // our dot-products of our values in registers.
+      for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
+        for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+          accumulator[i][j] += a_frag[i] * b_frag[j];
+        }
+      }
+    }
+
+    __syncthreads();
+  }
+
+  // Write our values that are in our registers to GMEM.
+  // Important to write values coalesced by swapping the N and M loops
+  // to traverse column-wise first instead of row-wise
+  for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+    for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
+      int linear = ((blockIdx.y * BLOCK_TILE_Y) + (tid_y * NUM_TH_ITEMS_M + i)) * N + ((blockIdx.x * BLOCK_TILE_X) + (tid_x * NUM_TH_ITEMS_N + j));
+      c[linear] = accumulator[i][j];
+    }
+  }
+}
+
+/*
+  Building on our last function, we're now going to implement warp-tiling which is yet another level of tiling...
+  There aren't any "plain" CUDA-specific constructs to interact with warps and to achieve warp-tiling.
+  So we must modify the existing kernel in a way that makes sense for tiling along our hidden "warp" dimension.
+*/
+
+#define WARP_M 32
+#define WARP_N 16
+#define WARP_INNER_M 2
+#define WARP_INNER_N 2
+#define THREAD_M 2
+#define THREAD_N 2
+
+__global__ void sgemm_v5(const float *a, float alpha, const float *b, float beta, float *c, size_t M, size_t N, size_t K) {
+  __shared__ float ATile[BLOCK_TILE_Y][BLOCK_TILE_X];
+  __shared__ float BTile[BLOCK_TILE_Y][BLOCK_TILE_X];
+
+  // Block indices dictate the C-block we are going to process
+  // We still need to process an entire row of A and an entire column of B
+  int block_x = blockIdx.x;
+  int block_y = blockIdx.y;
+
   // Dictates which value of C we're computing within the C-block
   int tid_x = threadIdx.x;
   int tid_y = threadIdx.y;
 
-  int warp_id = threadIdx.x / WARP_SIZE;
-  int lane_id = threadIdx.x % WARP_SIZE;
+  int warp_id = (tid_x * tid_y) / WARP_SIZE;
+  int lane_id = (tid_x * tid_y) % WARP_SIZE;
+  int warp_x = tid_x / WARP_SIZE;
+  int warp_y = tid_y / WARP_SIZE;
 
   float accumulator[NUM_TH_ITEMS_M][NUM_TH_ITEMS_N] = {0.0};
   float a_frag[NUM_TH_ITEMS_M];
@@ -144,8 +242,8 @@ __global__ void sgemm_v4(const float *a, float alpha, const float *b, float beta
     const float *tile_a = &a[(block_y * BLOCK_TILE_X) * K + (step * BLOCK_TILE_X)];
     const float *tile_b = &b[(step * BLOCK_TILE_X) * N + (block_x * BLOCK_TILE_X)];
 
-    for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
-      for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+    for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+      for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
         ATile[tid_y + (j * blockDim.x)][tid_x + (i * blockDim.y)] = tile_a[(tid_y + (j * blockDim.x)) * K + ((tid_x + (i * blockDim.y)))];
         BTile[tid_y + (j * blockDim.x)][tid_x + (i * blockDim.y)] = tile_b[(tid_y + (j * blockDim.x)) * N + ((tid_x + (i * blockDim.y)))];
       }
@@ -154,11 +252,16 @@ __global__ void sgemm_v4(const float *a, float alpha, const float *b, float beta
     __syncthreads();
 
     for (int k = 0; k < BLOCK_TILE_X; ++k) {
-      for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
-        a_frag[i] = ATile[tid_y * NUM_TH_ITEMS_M + i][k];
+      for (int warp_m = 0; warp_m < WARP_INNER_M; ++warp_m) {
+        for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
+          a_frag[i] = ATile[tid_y * NUM_TH_ITEMS_M + i][k];
+        }
       }
-      for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
-        b_frag[j] = BTile[k][tid_x * NUM_TH_ITEMS_N + j];
+
+      for (int warp_n = 0; warp_n < WARP_INNER_N; ++warp_n) {
+        for (int j = 0; j < NUM_TH_ITEMS_N; ++j) {
+          b_frag[j] = BTile[k][tid_x * NUM_TH_ITEMS_N + j];
+        }
       }
 
       for (int i = 0; i < NUM_TH_ITEMS_M; ++i) {
