078aa54510fc5227531c736c5ca7cae01eb6b3b5
brod4910 <brod4910@gmail.com>
2025-01-25 00:20:41 -0700

    fix indexing calculation and account for tile quantization

--- include/mlkl/cuda/operators/softmax.h
+++ include/mlkl/cuda/operators/softmax.h
@@ -9,14 +9,18 @@
 namespace ml::operators::cuda {
 namespace kernel {
 template<int NUM_THREADS>
-__global__ void softmax_2d_v1(float *input, float *output, int size) {
-  int tid = threadIdx.x;
+__global__ void softmax_2d_v1(float *input, float *output, int batch_size, int dim_size) {
+  int row = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (row > batch_size) {
+    return;
+  }
 
   float norm_factor = 0.f;
   float maximum = 0.f;
 
-  for (int i = 0; i < size; ++i) {
-    float value = input[tid * size + i];
+  for (int i = 0; i < dim_size; ++i) {
+    float value = input[row * dim_size + i];
     float new_max = fmax(value, maximum);
     float correction = expf(maximum - new_max);
 
@@ -24,19 +28,20 @@ __global__ void softmax_2d_v1(float *input, float *output, int size) {
     maximum = new_max;
   }
 
-  for (int i = 0; i < size; ++i) {
-    output[tid * size + i] = expf(input[tid * size + i] - maximum) / norm_factor;
+  for (int i = 0; i < dim_size; ++i) {
+    output[row * dim_size + i] = expf(input[row * dim_size + i] - maximum) / norm_factor;
   }
 }
 }// namespace kernel
 
 void launch_softmax_2d_v1(float *input, float *output, int dim, int *shape) {
   constexpr NUM_THREADS = 16;
-  int size = shape[dim];
+  int batch_size = shape[0];
+  int dim_size = shape[dim];
 
-  dim3 grid_dim(ceil_div(size, 16));
+  dim3 grid_dim(ceil_div(dim_size, 16));
   dim3 block_dim(NUM_THREADS);
 
-  kernel::softmax_2d_v1<NUM_THREADS><<<grid_dim, block_dim>>>(input, output, int dim, int size);
+  kernel::softmax_2d_v1<NUM_THREADS><<<grid_dim, block_dim>>>(input, output, batch_size, dim_size);
 }
 }// namespace ml::operators::cuda
