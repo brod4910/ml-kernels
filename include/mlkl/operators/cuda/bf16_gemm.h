d8541f7a4bafab6fd7678de65b8a68a2f6f4c266
Brian Rodriguez <brod4910@gmail.com>
2025-02-27 13:29:47 -0700

    adds v2 and partial v3 implementation

--- include/mlkl/operators/cuda/bf16_gemm.h
+++ include/mlkl/operators/cuda/bf16_gemm.h
@@ -4,5 +4,7 @@
 namespace mlkl::operators::cuda {
 void bf16_gemm_v1(Tensor *a, Tensor *b, Tensor *c, float alpha, float beta);
 
+void bf16_gemm_v2(Tensor *a, Tensor *b, Tensor *c, float alpha, float beta);
+
 void bf16_gemm(Tensor *a, Tensor *b, Tensor *c, float alpha, float beta);
 }// namespace mlkl::operators::cuda
