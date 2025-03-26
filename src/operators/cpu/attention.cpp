ffb8479bf6011826bcb860fa4e40850a6aa6d17f
brod4910 <brod4910@gmail.com>
2025-03-25 22:32:42 -0600

    partial cpu impl

--- src/operators/cpu/attention.cpp
+++ src/operators/cpu/attention.cpp
@@ -1,22 +1,56 @@
 #include <cmath>
-#include <mlkl/operators/cpu/gemm.h>
+#include <mlkl/operators/cpu/attention.h>
 
 namespace mlkl::operators::cpu {
 namespace {
+void softmax(const float *__restrict__ input, float *__restrict__ output, int batch_size, int num_heads, int seq_len_q, int seq_len_kv, int head_dim) {
+
+  for (int b = 0; b < batch_size; ++b) {
+    for (int head = 0; head < num_heads; ++head) {
+      for (int i = 0; i < seq_len_q; ++i) {
+        float curr_max = -std::numeric_limits<float>::infinity();
+        float norm_factor = 0.f;
+        for (int i = 0; i < seq_len_kv; ++i) {
+          float new_max = std::fmax(input[i], curr_max);
+          float correction = std::exp(curr_max - new_max);
+
+          norm_factor = (norm_factor * correction) + std::exp(input[i] - new_max);
+          curr_max = new_max;
+        }
+
+        for (int i = 0; i < seq_len_kv; ++i) {
+          output[b * num_heads * seq_len_q * seq_len_kvi] = std::exp(input[i] - curr_max) / norm_factor;
+        }
+      }
+    }
+  }
+}
+
 void attention(const float *__restrict__ q,
                const float *__restrict__ k,
                float *__restrict__ v,
                float *output,
-               int batch_size, int num_heads, int seq_len, int head_dim) {
-  float sqrt_hd = sqrtf((float) head_dim);
+               int batch_size, int num_heads, int seq_len_q, int seq_len_kv, int head_dim) {
+  float attn_scale = 1 / std::sqrt((float) head_dim);
+
+  float *scores = new float[seq_len_q * seq_len_kv]();
+  float *attn = new float[seq_len_q * seq_len_kv]();
 
   for (int b = 0; b < batch_size; ++b) {
-    for (int nh = 0; nh < num_heads; ++nh) {
-      for (int sl = 0; sl < seq_len; ++sl) {
-        for (int d = 0; d < head_dim; ++d) {
-          dot = q[b * num_heads * seq_len * head_dim + nh * seq_len * head_dim + sl * head_dim + d] * k[d * seq_len * num_heads * batch_size + sl * num_heads * batch_size + nh * batch_size + b];
+    for (int head = 0; head < num_heads; ++head) {
+      for (int slq = 0; slq < seq_len_q; ++slq) {
+        for (int slk = 0; slk < seq_len_kv; ++slk) {
+          for (int d = 0; d < head_dim; ++d) {
+            scores[slq * seq_len_kv + slk] += q[b * num_heads * seq_len_q * head_dim + head * seq_len_q * head_dim + slq * head_dim + d] * k[d * seq_len_kv * num_heads * batch_size + slk * num_heads * batch_size + head * batch_size + b];
+          }
         }
       }
+    }
+  }
+
+  for (int slq = 0; slq < seq_len_q; ++slq) {
+    for (int slk = 0; slk < seq_len_kv; ++slk) {
+      scores[slq * seq_len_kv + slk] *= attn_scale;
     }
   }
 }
@@ -25,9 +59,10 @@ void attention(const float *__restrict__ q,
 void attention(Tensor *q, Tensor *k, Tensor *v, Tensor *output) {
   int batch_size = q->shape[0];
   int num_heads = q->shape[1];
-  int seq_len = q->shape[2];
+  int seq_len_q = q->shape[2];
+  int seq_len_k = k->shape[2];
   int head_dim = q->shape[3];
 
-  attention(q->fp32_(), k->fp32_(), v->fp32_(), output->fp32_(), batch_size, num_heads, seq_len, head_dim);
+  attention(q->fp32_(), k->fp32_(), v->fp32_(), output->fp32_(), batch_size, num_heads, seq_len_q, seq_len_k, head_dim);
 }
 }// namespace mlkl::operators::cpu
--- src/operators/cuda/attention.cu
+++ src/operators/cuda/attention.cu
@@ -1,3 +1,4 @@
+#include "mlkl/core/tensor.h"
 #include <cstddef>
 #include <cstdio>
 
@@ -16,54 +17,23 @@ using namespace nvcuda;
 namespace mlkl::operators::cuda {
 namespace kernel {
 // naive
-template<int WM, int WN, int WK>
-__global__ __launch_bounds__(32) void attention_v1(const fp32 *q, const fp32 *k, fp32 *v, size_t M, size_t N, size_t K) {
-  // suppose we are processing block (2, 3) and WM = WN = WK = 16
-  // warp_col = 2 * WN = 2 * 16 = 32
-  // warp row = 3 * WN = 3 * 16 = 48
-  //
-  // block (7, 7)
-  // warp_col = 7 * WN = 7 * 16 = 112
-  // warp row = 7 * WN = 7 * 16 = 112
-  int warp_row = blockIdx.y * WM;
-  int warp_col = blockIdx.x * WN;
-
-  wmma::fragment<wmma::matrix_a, WM, WN, WK, bf16, wmma::row_major> a_frag;
-  wmma::fragment<wmma::matrix_b, WM, WN, WK, bf16, wmma::row_major> b_frag;
-  wmma::fragment<wmma::accumulator, WM, WN, WK, float> accumulator;
-  wmma::fill_fragment(accumulator, 0.);
-
-  for (int step = 0; step < K / WK; ++step) {
-
-    wmma::load_matrix_sync(a_frag, &a[(warp_row * WM) * K + (step * WK)], K);
-    wmma::load_matrix_sync(b_frag, &b[(step * WK) * N + (warp_col * WN)], N);
-
-    wmma::mma_sync(accumulator, a_frag, b_frag, accumulator);
-  }
-
-  wmma::store_matrix_sync(&c[warp_row * N + warp_col], accumulator, N, wmma::mem_row_major);
+__global__ __launch_bounds__(32) void attention_v1(const fp32 *q, const fp32 *k, const fp32 *v, fp32 *output, int batch_size, int num_heads, int seq_len, int head_dim) {
 }
 }// namespace kernel
 
 namespace {
-void launch_attention_v1(const fp32 *a, float alpha, const fp32 *b, float beta, fp32 *c, size_t M, size_t N, size_t K) {
-  constexpr int WM = 16;
-  constexpr int WN = 16;
-  constexpr int WK = 16;
-
-  // if M = 128, M = 128
-  // (128 / WN, 128 / WM) = (128 / 16, 128 / 16)
-  // so we will launch a 8x8 grid where each block is comprised
-  // a collection of 32-threads
-  dim3 block_dim(WARP_SIZE);
-  dim3 grid_dim(math::ceil_div(N, WN), math::ceil_div(M, WM));
-
-  kernel::attention_v1<WM, WN, WK><<<grid_dim, block_dim>>>(a, alpha, b, beta, c, M, N, K);
+void launch_attention_v1(const fp32 *q, const fp32 *k, const fp32 *v, fp32 *output, int batch_size, int num_heads, int seq_len, int head_dim) {
+  // kernel::attention_v1<WM, WN, WK><<<grid_dim, block_dim>>>(q, k, v, output, batch_size, num_heads, seq_len, head_dim);
 }
 }// namespace
 
 void attention_v1(Tensor *q, Tensor *k, Tensor *v, Tensor *output) {
-  launch_attention_v1(q->fp32_(), k->fp32_(), v->fp32_(), c->shape[0], c->shape[1], a->shape[1]);
+  int batch_size = q->shape[0];
+  int num_heads = q->shape[1];
+  int seq_len = q->shape[2];
+  int head_dim = q->shape[3];
+
+  launch_attention_v1(q->fp32_(), k->fp32_(), v->fp32_(), output->fp32_(), batch_size, num_heads, seq_len, head_dim);
 }
 
 void attention(Tensor *q, Tensor *k, Tensor *v, Tensor *output) {
